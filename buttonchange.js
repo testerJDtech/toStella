6f811db1b171316ed83ffc3ebb70b4d1d117fb5f
John Dada <johndada@Mac.home>
2025-05-24 23:29:07 +0100

    finishBtn javascript

--- buttonchange.js
+++ buttonchange.js
@@ -1,10 +1,18 @@
 let currentBlock = 0;
 const blocks = document.querySelectorAll('.block');
+const finishBtn = document.getElementById('finishBtn');
 
 function showBlock(index) {
     blocks.forEach((block, i) => {
         block.classList.toggle('active', i === index);
     });
+
+    if (index === blocks.length - 1) {
+        finishBtn.style.display = 'block';
+    }
+    else {
+        finishBtn.style.display = 'none';
+    }
 }
 
 function showNextBlock() {
@@ -16,6 +24,8 @@ function showPrevBlock() {
     currentBlock = (currentBlock - 1 + blocks.length) % blocks.length;
     showBlock(currentBlock);
 }
+
+// function 
 
 // Initialize the first block as visible
 showBlock(currentBlock);
